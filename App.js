4d3ea403b8bbf104f59caeaf595175f3382515d5
O-imperador <1209iyed@gmail.com>
2025-12-09 04:37:13 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import Navigation from './src/navigation'; // Navigation de l'application
+import { Provider } from 'react-redux'; // Fournisseur Redux
+import store from './src/store'; // Importation du store Redux
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Navigation />
+        <StatusBar style='auto' />
+      </View>
+    </Provider>
+  );
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+});
